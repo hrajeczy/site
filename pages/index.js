a63ffff135784478ef9b91dcf22f838d70f6a0d9
Zoltan Ray <zray@users.noreply.github.com>
2023-10-08 11:07:46 +0100

    more

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1>Coming soon</h1>
+        <h1>Helen Ray</h1>
+        <h2>Coming soon</h2>
       </main>
     </>
   )
